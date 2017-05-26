d61700af93568ac9dc826d0296dc640603a4f6a4
Bailig Abhanar <barrybailig@hotmail.com>
2017-05-25 20:33:49 -0400

    Completed

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 
 $(document).ready(function() {
-
+    init();
+    handelNavigation();
     spyScrollAndToggleSidebarFix();
-
 });
 
+function init() {
+    $("#main").load("partial-views/overview.html");
+    $(".nav-btn:first").addClass("active");
+}
+
+function handelNavigation() {
+    $(".nav-btn").on("click", function() {
+        havdelActiveNavigationButton(this);
+        var fileName = "partial-views/" + this.innerText.toLowerCase() + ".html";
+        $("#main").load(fileName);
+    });
+}
+
+function havdelActiveNavigationButton(navBtn) {
+    $(".navbar").find("a.nav-btn").removeClass("active");
+    $(navBtn).addClass("active");
+}
 
 function spyScrollAndToggleSidebarFix() {
     $(window).on("scroll", function() {
